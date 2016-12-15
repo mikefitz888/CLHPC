be97fcb90848656e951dfb3f624719aa9fb279e1
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-15 17:13:53 +0000

    more opencl kernel jazz

--- opencllbm.c
+++ opencllbm.c
@@ -238,7 +238,7 @@ int main(int argc, char* argv[])
 
   gettimeofday(&timstr, NULL);
   toc = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
-  getrusage(RUSAGE_SELF, &ru);
+  //getrusage(RUSAGE_SELF, &ru);
   //timstr = ru.ru_utime;
   /*usrtim = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
   timstr = ru.ru_stime;
