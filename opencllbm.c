caa51e3d59adfd7270025faaa6121685ea57956b
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-16 21:08:42 +0000

    kernel attempt #1

--- opencllbm.c
+++ opencllbm.c
@@ -407,7 +407,7 @@ int timestep(const t_param* restrict params, t_speed* cells, t_speed* tmp_cells,
     err = clFinish(ocl.queue);
     checkError(err, "waiting for lbm kernel", __LINE__);*/
 
-    err = clSetKernelArg(ocl.lbm, 0, sizeof(cl_mem), &ocl.tmp_cells);
+    /*err = clSetKernelArg(ocl.lbm, 0, sizeof(cl_mem), &ocl.tmp_cells);
     checkError(err, "setting lbm arg 0", __LINE__);
     err = clSetKernelArg(ocl.lbm, 1, sizeof(cl_mem), &ocl.cells);
     checkError(err, "setting lbm arg 1", __LINE__);
@@ -416,7 +416,7 @@ int timestep(const t_param* restrict params, t_speed* cells, t_speed* tmp_cells,
     err = clEnqueueNDRangeKernel(ocl.queue, ocl.lbm, 2, NULL, global, NULL, 0, NULL, NULL);
     checkError(err, "enqueuing lbm kernel", __LINE__);
     err = clFinish(ocl.queue);
-    checkError(err, "waiting for lbm kernel", __LINE__);
+    checkError(err, "waiting for lbm kernel", __LINE__);*/
 
     // Reposition top/bottom "ghost" rows
     /*err = clSetKernelArg(ocl.swapGhostCellsTB, 0, sizeof(cl_mem), &ocl.grid);
