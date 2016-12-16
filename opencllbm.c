057a3f9c8854cbce0fe70164130645ca8409e4ed
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-16 18:41:21 +0000

    kernel attempt #1

--- opencllbm.c
+++ opencllbm.c
@@ -243,7 +243,7 @@ int main(int argc, char* argv[])
   checkError(err, "writing obstacles data", __LINE__);
  
   //Lattice-Bolzmann Iterations (this function contains the loop, no need to loop this call)
-  //timestep(params, cells+offset, tmp_cells+offset, obstacles, av_vels, 1.0/available_cells, ocl);
+  timestep(params, cells+offset, tmp_cells+offset, obstacles, av_vels, 1.0/available_cells, ocl);
 
   //TODO: Pass chunks back to master from other nodes
   // Read tmp_cells from device                                   
