ad7f8244936e46533abeafecb6f6874ec676b83c
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-16 04:04:44 +0000

    kernel attempt #1

--- opencllbm.c
+++ opencllbm.c
@@ -767,8 +767,8 @@ int initialise(const char* paramfile, const char* obstaclefile,
   }
   checkError(err, "building program", __LINE__);
 
-  cl_buffer_region     cells_sub_buffer = { 4*sizeof(t_speed), sizeof(t_speed)*(9*params->nx_pad)*params->ny_pad*2};
-  cl_buffer_region tmp_cells_sub_buffer = { ((9*params->nx_pad)+4)*sizeof(t_speed), sizeof(t_speed)*(9*params->nx_pad)*(params->ny_pad*2 - 1)};
+  cl_buffer_region     cells_sub_buffer = { 4*sizeof(cl_float), sizeof(cl_float)*(9*params->nx_pad)*params->ny_pad*2};
+  cl_buffer_region tmp_cells_sub_buffer = { ((9*params->nx_pad)+4)*sizeof(cl_float), sizeof(cl_float)*(9*params->nx_pad)*(params->ny_pad*2 - 1)};
 
   // Create OpenCL kernels
   ocl->accelerate_flow = clCreateKernel(ocl->program, "accelerate_flow", &err);
