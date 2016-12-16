a9ffc0685bc5e78afcfc2a22d922f276740fc498
Michael Fitzpatrick <mikefitz888@gmail.com>
2016-12-16 23:45:55 +0000

    kernel attempt #1

--- opencllbm.c
+++ opencllbm.c
@@ -210,70 +210,7 @@ int main(int argc, char* argv[])
   
   // DOESN'T NEED MPI SPECIALISATION AS OUTSIDE TIMED REGION, WOULD BE BENEFICIAL OTHERWISE
   initialise(paramfile, obstaclefile, params, &cells, &tmp_cells, &obstacles, &av_vels, &available_cells, &ocl);
-  params->available_cells = available_cells;
-  t_speed *av_vels_recv = malloc(sizeof *av_vels_recv * (params->maxIters+1));
-  //printf("available cells = %d\n", available_cells);
-  int offset = 0;//4 + (2 * 9 * (params->nx_pad));
-  //printf("P%d sample cell value: %f\n", params->rank, cells[0 * params->ny * params->nx_pad + 5   * params->nx_pad + 5]);
-
-  //Ensure same start state ()
-  // DOESN'T NEED MPI SPECIALISATION AS OUTSIDE TIMED REGION, WOULD BE BENEFICIAL OTHERWISE
-  accelerate_flow(params, cells+offset, obstacles);
-  // DOESN'T NEED MPI SPECIALISATION AS OUTSIDE TIMED REGION, WOULD BE BENEFICIAL OTHERWISE
-  propagate(params, cells+offset, tmp_cells+offset);
-
-  /* iterate for maxIters timesteps */
-    gettimeofday(&timstr, NULL);
-    tic = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
-
-
-/*
-**  BEGIN TIMING STEP
-*/
-    cl_int err;
-  //Write cells to kernel
-  err = clEnqueueWriteBuffer(ocl.queue, ocl.tmp_cells, CL_TRUE, 0, sizeof(cl_double) * (9 * params->ny * params->nx), tmp_cells, 0, NULL, NULL);
-  checkError(err, "writing cells data", __LINE__);
-  err = clEnqueueWriteBuffer(ocl.queue, ocl.cells, CL_TRUE, 0, sizeof(cl_double) * (9 * params->ny * params->nx), cells, 0, NULL, NULL);
-  checkError(err, "writing cells data", __LINE__);
-  err = clEnqueueWriteBuffer(ocl.queue, ocl.obstacles, CL_TRUE, 0, sizeof(cl_float) * params->nx * params->ny, obstacles, 0, NULL, NULL);
-  checkError(err, "writing obstacles data", __LINE__);
- 
-  //Lattice-Bolzmann Iterations (this function contains the loop, no need to loop this call)
-  timestep(params, cells+offset, tmp_cells+offset, obstacles, av_vels, 1.0/available_cells, ocl);
-
-  //TODO: Pass chunks back to master from other nodes
-  // Read tmp_cells from device                                   
-  err = clEnqueueReadBuffer(ocl.queue, ocl.cells, CL_TRUE, 0, sizeof(cl_double) * (9 * params->ny * params->nx), cells, 0, NULL, NULL);
-  checkError(err, "reading tmp_cells data", __LINE__);
-  err = clEnqueueReadBuffer(ocl.queue, ocl.tmp_cells, CL_TRUE, 0, sizeof(cl_double) * (9 * params->ny * params->nx), tmp_cells, 0, NULL, NULL);
-  checkError(err, "reading tmp_cells data", __LINE__);
-
-
-/*
-**  END TIMING STEP
-*/
-
-  gettimeofday(&timstr, NULL);
-  toc = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
-  /*getrusage(RUSAGE_SELF, &ru);
-  timstr = ru.ru_utime;
-  usrtim = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
-  timstr = ru.ru_stime;
-  systim = timstr.tv_sec + (timstr.tv_usec / 1000000.0);*/
-
-  t_speed reynold_sum = calc_reynolds(params, cells+offset, obstacles);
-
-  /* write final values and free memory */
-  printf("==done==\n");
-  printf("Reynolds number:\t\t%.12E\n", reynold_sum); //TODO: make calc_reynolds MPI
-  printf("Elapsed time:\t\t\t%.6lf (s)\n", toc - tic);
-  //printf("Elapsed user CPU time:\t\t%.6lf (s)\n", usrtim);
-  //printf("Elapsed system CPU time:\t%.6lf (s)\n", systim);
   
-  write_values(params, tmp_cells+offset, obstacles, av_vels);
-
-  finalise(params, &cells, &tmp_cells, &obstacles, &av_vels, ocl);
   return EXIT_SUCCESS;
 }
 
